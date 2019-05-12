03b1e89cec6a0c2a82d126e4497d361009a7a55b
Cristian Gonzalez Morante <cristian.gonzalez.morante@gmail.com>
2019-05-12 12:24:52 +0200

    Optimization. BigInteger removed, so much faster with primitives

--- src/com/cristianroot/Main.java
+++ src/com/cristianroot/Main.java
@@ -1,28 +1,24 @@
 package com.cristianroot;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
-	private static final int NUMBER = 20;
-	private static final BigInteger[] summation = new BigInteger[10];
-	private static Map<Long, BigInteger> decResultStack = new HashMap<>();
-	private static Map<Long, BigInteger> incResultStack = new HashMap<>();
+	private static final int NUMBER = 30;
+	private static final long[] summation = new long[10];
 
 	public static void main(String[] args) {
 		initializeSummations();
 
 		long time = System.currentTimeMillis();
-		BigInteger totalInc = totalInc(NUMBER);
-		System.out.println("INC -- " + totalInc.toString() + ", " + (System.currentTimeMillis() - time) + "ms");
+		long totalInc = totalInc(NUMBER);
+		System.out.println("INC -- " + totalInc + ", " + (System.currentTimeMillis() - time) + "ms");
 
 		time = System.currentTimeMillis();
-		BigInteger totalDec = totalDec(NUMBER);
-		System.out.println("DEC -- " + totalDec.toString() + ", " + (System.currentTimeMillis() - time) + "ms");
+		long totalDec = totalDec(NUMBER);
+		System.out.println("DEC -- " + totalDec + ", " + (System.currentTimeMillis() - time) + "ms");
 
-		System.out.println("TOTAL -- " + (totalDec.add(totalInc)));
+		System.out.println("TOTAL -- " + (totalDec + totalInc));
 	}
 
 	private static void initializeSummations() {
@@ -31,72 +27,61 @@ public class Main {
 		}
 	}
 
-	private static BigInteger totalDec(int pow) {
-		BigInteger count = BigInteger.ZERO;
+	private static long totalDec(int pow) {
+		long count = 0;
 
 		int recursionLevel = pow - 2;
 		for (int i = 0; i < recursionLevel; i++) {
-			count = count.add(countDecreaseNumbers(0, i));
+			count += countDecreaseNumbers(0, i);
 		}
 
 		return count;
 	}
 
-	private static BigInteger totalInc(int pow) {
+	private static long totalInc(int pow) {
 		if (pow <= 2)
-			return BigInteger.TEN.pow(pow);
+			return BigInteger.TEN.pow(pow).longValue();
 
-		BigInteger count = BigInteger.valueOf(100L);
+		long count = 100;
 		int recursionLevel = pow - 2;
 
 		for (int i = 0; i < recursionLevel; i++) {
-			count = count.add(countIncreaseNumbers(0, i));
+			count += countIncreaseNumbers(0, i);
 		}
 
 		return count;
 	}
 
-	private static BigInteger summation(BigInteger n) {
-		return n.pow(2).add(n).divide(BigInteger.valueOf(2L));
+	private static long summation(BigInteger n) {
+		return n.pow(2).add(n).divide(BigInteger.valueOf(2L)).longValue();
 	}
 
-	private static BigInteger countIncreaseNumbers(long start, int recursionLevel) {
-		BigInteger count = BigInteger.ZERO;
+	private static long countIncreaseNumbers(int start, int recursionLevel) {
+		long count = 0;
 
 		if (recursionLevel == 0) {
-			if (incResultStack.containsKey(start))
-				return incResultStack.get(start);
-
-			for (long i = start; i < 9; i++) {
-				count = count.add(summation[(int) (8 - i)]);
+			for (int i = start; i < 9; i++) {
+				count += summation[8 - i];
 			}
-
-			incResultStack.put(start, count);
 		} else {
-			for (long i = start; i < 9; i++) {
-				count = count.add(countIncreaseNumbers(i, recursionLevel - 1));
+			for (int i = start; i < 9; i++) {
+				count += countIncreaseNumbers(i, recursionLevel - 1);
 			}
 		}
 
 		return count;
 	}
 
-	private static BigInteger countDecreaseNumbers(long start, int recursionLevel) {
-		BigInteger count = BigInteger.ZERO;
+	private static long countDecreaseNumbers(int start, int recursionLevel) {
+		long count = 0;
 
 		if (recursionLevel == 0) {
-			if (decResultStack.containsKey(start))
-				return decResultStack.get(start);
-
 			for (int i = 2; i <= 10 - start; i++) {
-				count = count.add(summation[i - 1].subtract(BigInteger.ONE));
+				count += summation[i - 1] - 1;
 			}
-
-			decResultStack.put(start, count);
 		} else {
-			for (long i = start; i < 9; i++) {
-				count = count.add(countDecreaseNumbers(i, recursionLevel - 1))
-							 .add(BigInteger.valueOf(9 - i));
+			for (int i = start; i < 9; i++) {
+				count = count + countDecreaseNumbers(i, recursionLevel - 1) + 9 - i;
 			}
 		}
 
